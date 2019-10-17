2bc7373fe30d58e1c7214afd66a7248ca3b3c9f9
Yurj Tugolukov <yurj9999@yandex.ru>
2019-10-17 22:29:48 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import {api} from './scripts/script.js';
-
-const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort3' : 'https://praktikum.tk/cohort3';
-
-api.getProfile();
-api.getCards();
-
-export {serverUrl};
